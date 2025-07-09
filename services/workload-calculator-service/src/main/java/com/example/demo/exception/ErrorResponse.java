470a14f48747af85e90f223a5c8899e798d1909c
asror812 <asrorruzimurodov812@gmail.com>
2025-07-09 20:43:18 +0500

    I think i finally did it

--- services/workload-calculator-service/src/main/java/com/example/demo/exception/ErrorResponse.java
+++ services/workload-calculator-service/src/main/java/com/example/demo/exception/ErrorResponse.java
@@ -7,12 +7,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class ErrorResponse {
 
     private String message;
